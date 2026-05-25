a550e3ce955dd2f3610629e4979f872db879982c
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-05-25 18:51:25 -0300

    Delete examples/configs/t_dongle_s3 directory

--- examples/configs/t_dongle_s3/tft_buttons.py
+++
@@ -1,8 +0,0 @@
-# input pin for esp32 T-DONGLE-S3 module
-
-from machine import Pin
-
-class Buttons():
-    def __init__(self):
-        self.name = "t-dongle-s3"
-        self.button = Pin(0, Pin.IN)
