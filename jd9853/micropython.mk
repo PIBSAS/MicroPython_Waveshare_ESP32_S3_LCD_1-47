0138b782e39aa5aaee7bf8f5499548a26d3fb380
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-05-25 14:07:13 -0300

    Update micropython.mk

--- jd9853/micropython.mk
+++ jd9853/micropython.mk
@@ -1,9 +1,9 @@
-ST7789_MOD_DIR := $(USERMOD_DIR)
+JD9853_MOD_DIR := $(USERMOD_DIR)
 
-CFLAGS_USERMOD += -I$(ST7789_MOD_DIR)
+CFLAGS_USERMOD += -I$(JD9853_MOD_DIR)
 
-SRC_USERMOD += $(addprefix $(ST7789_MOD_DIR)/, st7789.c)
-SRC_USERMOD += $(addprefix $(ST7789_MOD_DIR)/, mpfile.c)
-SRC_USERMOD += $(addprefix $(ST7789_MOD_DIR)/jpg/, tjpgd565.c)
-SRC_USERMOD += $(addprefix $(ST7789_MOD_DIR)/png/, pngle.c)
-SRC_USERMOD += $(addprefix $(ST7789_MOD_DIR)/png/, miniz.c)
+SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/, jd9853.c)
+SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/, mpfile.c)
+SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/jpg/, tjpgd565.c)
+SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/png/, pngle.c)
+SRC_USERMOD += $(addprefix $(JD9853_MOD_DIR)/png/, miniz.c)
