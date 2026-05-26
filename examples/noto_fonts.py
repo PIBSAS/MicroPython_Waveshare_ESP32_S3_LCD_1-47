424dee02f068e060622f10ee139adfc47da4977d
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-05-26 16:20:23 -0300

    Update noto_fonts.py

--- examples/noto_fonts.py
+++ examples/noto_fonts.py
@@ -4,7 +4,7 @@ noto_fonts Writes the names of three Noto fonts centered on the display
     the font2bitmap utility.
 """
 
-import st7789
+import jd9853
 import tft_config
 import NotoSans_32 as noto_sans
 import NotoSerif_32 as noto_serif
@@ -14,7 +14,7 @@ import NotoSansMono_32 as noto_mono
 tft = tft_config.config(1, buffer_size=16*32*2)
 
 
-def center(font, s, row, color=st7789.WHITE):
+def center(font, s, row, color=jd9853.WHITE):
     screen = tft.width()                     # get screen width
     width = tft.write_len(font, s)           # get the width of the string
     if width and width < screen:             # if the string < display
@@ -28,19 +28,19 @@ def center(font, s, row, color=st7789.WHITE):
 def main():
     # init display
     tft.init()
-    tft.fill(st7789.BLACK)
+    tft.fill(jd9853.BLACK)
 
     # center the name of the first font, using the font
     row = 16
-    center(noto_sans, "NotoSans", row, st7789.RED)
+    center(noto_sans, "NotoSans", row, jd9853.RED)
     row += noto_sans.HEIGHT
 
     # center the name of the second font, using the font
-    center(noto_serif, "NotoSerif", row, st7789.GREEN)
+    center(noto_serif, "NotoSerif", row, jd9853.GREEN)
     row += noto_serif.HEIGHT
 
     # center the name of the third font, using the font
-    center(noto_mono, "NotoSansMono", row, st7789.BLUE)
+    center(noto_mono, "NotoSansMono", row, jd9853.BLUE)
     row += noto_mono.HEIGHT
 
 
